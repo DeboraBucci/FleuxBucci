e89e17d0c9eb45ad67e9f0f90c8d5e069dfb6dff
Debora Bucci <dbucci@protonmail.com>
2022-10-12 15:04:00 -0300

    make sidebar interactive

--- scripts/categories-sidebar.js
+++ scripts/categories-sidebar.js
@@ -1,19 +1,64 @@
 const categoriesEl = document.querySelectorAll(".sidebar__item");
 
-// OPEN AND CLOSE ITEMS
+const categoriesOptions = {
+  all: [
+    "SEE ALL",
+    "CHAIRS",
+    "TABLES",
+    "POTS",
+    "PAINTS",
+    "DECORATION",
+    "LIGHTS",
+  ],
+  kids: ["BEDS", "CRIBS"],
+};
+
+const changeArrowDirection = (el, add = "up", remove = "down") => {
+  el.classList.add(`fa-chevron-${add}`);
+  el.classList.remove(`fa-chevron-${remove}`);
+};
+
 categoriesEl.forEach((el) => {
   el.addEventListener("click", (e) => {
-    const allItemEl = categoriesEl;
     const selectedParent = e.target.closest("li");
+    const iconEl = selectedParent
+      .querySelector(".sidebar__item")
+      .querySelector(".fa-solid");
 
     if (selectedParent.classList.contains("active-li")) {
       selectedParent.classList.remove("active-li");
+      selectedParent.removeChild(document.querySelector(".sidebar__item-list"));
+      changeArrowDirection(iconEl, "down", "up");
       return;
     }
 
-    allItemEl.forEach((item) => {
-      item.closest("li").classList.remove("active-li");
+    categoriesEl.forEach((item) => {
+      const parentLi = item.closest("li");
+      parentLi.classList.remove("active-li");
+      changeArrowDirection(item.querySelector(".fa-solid"), "down", "up");
+
+      if (parentLi.querySelector(".sidebar__item-list") !== null) {
+        parentLi.removeChild(document.querySelector(".sidebar__item-list"));
+      }
     });
+
     selectedParent.classList.add("active-li");
+    selectedParent.insertAdjacentHTML(
+      "beforeend",
+      `<ul class='sidebar__item-list'>${categoriesOptions.all
+        .map((opt) => `<li class='sidebar__option'>${opt}</li>`)
+        .join("")}</ul>`
+    );
+    changeArrowDirection(iconEl);
+
+    const optionsEl = selectedParent.querySelectorAll(".sidebar__option");
+    optionsEl.forEach((option) =>
+      option.addEventListener("click", () => {
+        optionsEl.forEach((opt) =>
+          opt.classList.remove("sidebar__option--active")
+        );
+        option.classList.add("sidebar__option--active");
+      })
+    );
   });
 });
